2c1e6ab8380157389ba04b575c5484933c0ce920
Luis Diego Gomez Fajardo <107708809+SourcenXY@users.noreply.github.com>
2026-07-11 23:30:08 -0500

    Delete PA4 directory

--- PA4/code/Soporte.js
+++
@@ -1,9 +0,0 @@
-//==============================
-// Página de Soporte Epic Games
-//==============================
-
-document.addEventListener("DOMContentLoaded", function(){
-
-    console.log("Bienvenido a Epic Games.");
-
-});
